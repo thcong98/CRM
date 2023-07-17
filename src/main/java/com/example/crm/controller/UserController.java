44fe10afe0fc4a24bb935aeafffbb302c9443d48
Dawn Hiu <113577868+HieuDang2000@users.noreply.github.com>
2023-07-17 15:31:58 +0700

    Update UserController.java

--- src/main/java/com/example/crm/controller/UserController.java
+++ src/main/java/com/example/crm/controller/UserController.java
@@ -1,9 +1,5 @@
 package com.example.crm.controller;
 
 public class UserController {
-    String he = "hehe";
-    String he2= "haha";
-    String he4= "kkkk";
-    String he3 = "hoho";
-     String he5 = "lolo";
+  
 }
